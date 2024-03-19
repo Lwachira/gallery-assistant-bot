ee3311745bd21d03560add941295916fd0a8d607
Lincoln Gachagua <blazeboy.wachira@gmail.com>
2024-03-19 20:10:23 +0200

    x

--- src/bot/features/open-ai-test.ts
+++ src/bot/features/open-ai-test.ts
@@ -1,26 +1,24 @@
 import { Composer } from "grammy";
 import type { Context } from "#root/bot/context.js";
 import { logHandle } from "#root/bot/helpers/logging.js";
-import OpenAI from "openai";
-import { config } from "#root/config.js";
+// import OpenAI from "openai";
+// import { config } from "#root/config.js";
 
-const openai = new OpenAI({
-  apiKey: config.OPENAI_API_KEY,
-});
+// // const openai = new OpenAI({
+// //   apiKey: config.OPENAI_API_KEY,
+// // });
 
-const chatCompletion = await openai.chat.completions.create({
-  messages: [{ role: "user", content: "Say this is a test" }],
-  model: "gpt-3.5-turbo",
-});
+// // const chatCompletion = await openai.chat.completions.create({
+// //   messages: [{ role: "user", content: "Say this is a test" }],
+// //   model: "gpt-3.5-turbo",
+// // });
+
 const composer = new Composer<Context>();
 
 const feature = composer.chatType("private");
 
 feature.command("aitest", logHandle("command-start"), async (ctx) => {
-  const response =
-    chatCompletion.choices.at(0)?.message.content?.toString() ??
-    "Default response if undefined or null";
-  return ctx.reply(ctx.t(response));
+  return ctx.reply(ctx.t("AI text goes here"));
 });
 
 export { composer as aiTestFeature };
